f4f9e81dacc8b83fe5e59aa5b50ad6b98c98765a
Dan <dchen12812@gmail.com>
2015-07-05 19:54:35 -0700

    modified layouts, added gravatar link for edit user, begun work in post controller/model and testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Users
+User.create!(name: "Boo Mc.Boo",
+	         email: "example@test.com",
+	         password: "testtest",
+	         password_confirmation: "testtest")
