e795e78c6a8ef1b14be01f78dd8b8bffdc5aa512
Dan <dchen12812@gmail.com>
2015-07-05 22:43:17 -0700

    preliminary index and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
+  get 'users/:id' => 'users#show'
 
+  resources :users, only: [:index, :show]
 
   root 'static_pages#index'
 
