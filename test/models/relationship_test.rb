2cdc28dc8194d72a7a20f8698104e0be4f695953
Dan <dchen12812@gmail.com>
2015-07-15 00:09:31 -0700

    added testing for user model

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,6 +1,24 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
+  def setup
+    @friend = Relationship.new(user_id: 1, friend_id: 2)
+  end
+
+  test 'should be valid' do
+    assert @friend.valid?
+  end
+
+  test 'user_id should be present' do
+    @friend.user_id = " "
+    assert_not @friend.valid?
+  end
+
+  test 'friend_id should be present' do
+    @friend.friend_id = " "
+    assert_not @friend.valid?
+  end
+
   test "should friend and unfriend a user" do
     test1  = users(:user1)
     test2   = users(:user2)
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,44 @@ class UserTest < ActiveSupport::TestCase
 	test 'not invited' do
 		assert_not @user_one.invited?(@user_two)
 	end
+
+	test 'invited' do
+		@user_one.invite(@user_two)
+		assert @user_one.invited?(@user_two)
+	end
+
+	test 'reject invite' do
+		@user_one.invite(@user_two)
+		assert_difference 'Invite.count', -1 do
+			@user_two.reject(@user_one)
+		end
+		assert_not @user_one.invited?(@user_two)
+	end
+
+	test 'not friends' do
+		@user_one.friends?(@user_two)
+	end
+
+	test 'friends' do
+		assert_difference 'Relationship.count', 2 do
+			@user_one.friend(@user_two)
+		end
+		assert @user_one.friends?(@user_two)
+		assert @user_two.friends?(@user_one)
+	end
+
+	test 'can unfriend' do
+		assert_difference 'Relationship.count', 2 do
+			@user_one.friend(@user_two)
+		end
+		assert @user_one.friends?(@user_two)
+		assert @user_two.friends?(@user_one)
+
+		assert_difference 'Relationship.count', -2 do
+			@user_one.unfriend(@user_two)
+		end
+		assert_not @user_one.friends?(@user_two)
+		assert_not @user_two.friends?(@user_one)
+	end
+
 end
