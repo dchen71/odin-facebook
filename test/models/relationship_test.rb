a15b190e78450098e6bd64669623a68fed545f0a
Dan <dchen12812@gmail.com>
2015-07-14 23:43:24 -0700

    added some tests

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  test "should follow and unfollow a user" do
-    test1  = users(:test1)
-    test2   = users(:test2)
+  test "should friend and unfriend a user" do
+    test1  = users(:user1)
+    test2   = users(:user2)
     assert_not test1.friends?(test2)
     test1.friend(test2)
     assert test1.friends?(test2)
-    assert test2.friends.include?(test1)
+    assert test2.friends?(test1)
     test1.unfriend(test2)
     assert_not test1.friends?(test2)
   end
