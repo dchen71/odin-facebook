aee56a5929287259fcaa1008461e0cfc9da91841
Dan <dchen12812@gmail.com>
2015-07-14 23:21:15 -0700

    bug fix of name and email field for user edit

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+	def setup
+		@user_one = users(:user1)
+		@user_two = users(:user2)
+	end
+
+	test 'not invited' do
+		assert_not @user_one.invited?(@user_two)
+	end
 end
