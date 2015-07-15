b4074ff322d08123e87984451df2d3d282c6e56f
Dan <dchen12812@gmail.com>
2015-07-14 22:27:46 -0700

    implemented feed with desc order on feed page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 	before_action :correct_user, only: [:edit, :update, :destroy]
 
 	def index
-		@posts = current_user.posts.paginate(page:params[:page],per_page:10)
+		@posts = current_user.feed.reorder("created_at DESC").paginate(page:params[:page],per_page:10)
 		@post = Post.new
 	end
 
