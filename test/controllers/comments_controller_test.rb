fa88245bec1201b1427d859619a7ab3651bef0f4
Dan <dchen12812@gmail.com>
2015-07-20 01:07:42 -0700

    updated user test and added comment controller tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@comment = comments(:com1)
+  	@user = users(:user3)
+  end
+
+  test 'redirect from create if not logged in' do
+  	patch :update, id: @comment, comment: {content: @comment.content, 
+  										   post_id: @comment.post_id, user_id: @comment.user_id}
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'redirect from edit if not logged in' do
+  	get :edit, id: @comment
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'redirect destroy if not logged in' do
+  	delete :destroy, id: @comment
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'redirect create if not logged in' do
+  	post :update, id: @comment, comment: {content: @comment.content, 
+  										   post_id: @comment.post_id, user_id: @comment.user_id}
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'redirect update if incorrect user' do
+  	sign_in @user
+  	patch :update, id: @comment, comment: {content: @comment.content, 
+  										   post_id: @comment.post_id, user_id: @comment.user_id}
+    assert_redirected_to root_path
+  end
+
+  test 'redirect edit if incorrect user' do
+  	sign_in @user
+  	get :edit, id: @comment
+    assert_redirected_to root_path
+  end
+
+  test 'redirect destroy if incorrect user' do
+  	sign_in @user
+  	delete :destroy, id: @comment
+    assert_redirected_to root_path
+  end
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,10 +6,26 @@ class UsersControllerTest < ActionController::TestCase
   	@other = users(:user2)
   end
 
-  test 'should redirect when not logged in' do
+  test 'should redirect from index when not logged in' do
   	get :index
   	assert_redirected_to new_user_session_path
   end
+
+  test 'should redirect from show when not logged in' do
+    get :show, id: @user
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'should redirect from invites when not logged in' do
+    get :invites, id: @user
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'should redirect from friends when not logged in' do
+    get :friends, id: @user
+    assert_redirected_to new_user_session_path
+  end
+
 
   test 'should redirect when wrong user for invites' do
   	sign_in @other
