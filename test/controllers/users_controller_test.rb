8c25424b99e1207e1a162041bf50d7c7d9af89b6
Dan <dchen12812@gmail.com>
2015-07-20 00:51:30 -0700

    added tests for user controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = users(:user1)
+  	@other = users(:user2)
+  end
+
+  test 'should redirect when not logged in' do
+  	get :index
+  	assert_redirected_to new_user_session_path
+  end
+
+  test 'should redirect when wrong user for invites' do
+  	sign_in @other
+  	get :invites, id: @user
+  	assert_redirected_to root_path
+  end
 end
